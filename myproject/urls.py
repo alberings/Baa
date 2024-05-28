18136d114ef53e94eeec9db89a351a9237caf7a5
alberings <71380595+alberings@users.noreply.github.com>
2024-05-28 06:59:58 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Include built-in auth URLs
+    path('accounts/', include('django.contrib.auth.urls')),  
     
     
 ]
